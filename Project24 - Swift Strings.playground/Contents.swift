e50e9210a6c2268869aa62c3be7ddef6ae55f58d
Vitali <vitali.vyucheiski@gmail.com>
2022-06-13 11:18:45 +0300

    Initial commit

---
+++ Project24 - Swift Strings.playground/Contents.swift
@@ -0,0 +1,23 @@
+
+import UIKit
+
+let name = "Taylor"
+
+//for letter in name {
+//    print("Give me a \(letter)!")
+//}
+//
+//let letter = name[name.index(name.startIndex, offsetBy: 3)]
+//
+//extension String {
+//    subscript(i: Int) -> String {
+//        return String(self[index(startIndex, offsetBy: i)])
+//    }
+//}
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Project24---Swift-Strings
+ A Learning Project 24, coded by Me during Hacking with Swift in 100 Days Course
